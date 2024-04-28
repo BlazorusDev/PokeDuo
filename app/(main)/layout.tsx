0e24207452da38e9b0a6fcf94e2d45cf00116b91
Mohit-Mehra <info.mohit728@gmail.com>
2024-04-28 15:47:25 +0530

    main layout complete

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,3 +1,4 @@
+import { MobileHeader } from "@/components/mobile-header";
 import { Sidebar } from "@/components/sidebar";
 
 type Props = {
@@ -7,8 +8,9 @@ type Props = {
 const MainLayout = ({ children }: Props) => {
   return (
     <>
-      <Sidebar />
-      <main className="pl-[256px] h-full">
+      <MobileHeader />
+      <Sidebar className="hidden lg:flex" />
+      <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
         <div className="bg-red-500 h-full">{children}</div>
       </main>
     </>
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,6 +1,17 @@
-export const Sidebar = () => {
+import { cn } from "@/lib/utils";
+
+type Props = {
+  className?: string;
+};
+
+export const Sidebar = ({ className }: Props) => {
   return (
-    <div className="flex bg-blue-500 h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col">
+    <div
+      className={cn(
+        "flex bg-blue-500 h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
+        className
+      )}
+    >
       sidebar
     </div>
   );
