7d11c8e4f4f2d2073faaa75d4866016bdc950589
Mohit-Mehra <info.mohit728@gmail.com>
2024-05-03 16:22:40 +0530

    user progress complete

--- app/(main)/courses/card.tsx
+++ app/(main)/courses/card.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/lib/utils";
 import { Check } from "lucide-react";
+import Image from "next/image";
 
 type Props = {
   title: string;
@@ -33,6 +34,8 @@ export const Card = ({
           </div>
         )}
       </div>
+      <Image src={imageSrc} alt={title} height={70} width={93.33} className="rounded-lg drop-shadow-md border object-cover"/>
+      <p className="text-neutral-700 text-center font-bold mt-3">{title}</p>
     </div>
   );
 };
