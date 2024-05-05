eb18734c0b074003247143a311d2003d7eb7a027
Mohit-Mehra <info.mohit728@gmail.com>
2024-05-05 12:18:08 +0530

    schema complete

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -10,22 +10,22 @@ const LearnPage = async () => {
 
   const [userProgress] = await Promise.all([userProgressData]);
 
-  if(!userProgress || !userProgress.activeCourse){
-    redirect("/courses")
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
   }
 
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickWrapper>
         <UserProgress
-          activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }}
-          hearts={5}
-          points={100}
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
           hasActiveSubscription={false}
         />
       </StickWrapper>
       <FeedWrapper>
-        <Header title="Spanish" />
+        <Header title={userProgress.activeCourse.title} />
       </FeedWrapper>
     </div>
   );
