8e23472c1da08971bb7a305c4ed17814570f21fb
Mohit-Mehra <info.mohit728@gmail.com>
2024-05-12 21:47:06 +0530

    learn page start

---
+++ app/lesson/quiz.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { challengeOptions, challenges } from "@/db/schema";
+import { useState } from "react";
+
+type QuizProps = {
+  initialPercentage: number;
+  initialHearts: number;
+  initialLessonId: number;
+  initialLessonChallenges: (typeof challenges.$inferSelect & {
+    completed: boolean;
+    challengeOptions: (typeof challengeOptions.$inferSelect)[];
+  })[];
+  userSubscription: any;
+};
+
+export const Quiz = ({
+  initialPercentage,
+  initialHearts,
+  initialLessonId,
+  initialLessonChallenges,
+  userSubscription,
+}: QuizProps) => {
+  const [hearts, setHearts] = useState(initialHearts);
+  const [percentage, setPercentage] = useState(() => {
+    return initialPercentage === 100 ? 0 : initialPercentage;
+  });
+  
+  return (
+    <>
+      <Header
+        hearts={hearts}
+        percentage={percentage}
+        hasActiveSubscription={!!userSubscription?.isActive}
+      />
+    </>
+  );
+};
